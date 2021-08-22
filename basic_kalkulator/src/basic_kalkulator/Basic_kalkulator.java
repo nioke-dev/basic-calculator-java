964828ee8685cd3a250baa4ab9a98a2fb0dde8b7
Nurul Mustofa <66587167+nioke-dev@users.noreply.github.com>
2021-08-22 14:10:00 +0700

    Add files via upload

---
+++ basic_kalkulator/src/basic_kalkulator/Basic_kalkulator.java
@@ -0,0 +1,13 @@
+package basic_kalkulator;
+
+/**
+ *
+ * @author NIOKE
+ */
+public class Basic_kalkulator {
+
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
